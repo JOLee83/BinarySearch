0da644d70efddfc68858a74db4cf04dab0381b98
JOLee83 <lee.justin.oliver@gmail.com>
2018-10-22 17:19:03 -0400

    it works without recursion

--- Program.cs
+++ Program.cs
@@ -8,55 +8,48 @@ namespace BinarySearch
     {
       Console.WriteLine("Think of a number between 1 and 100. Ready? (Press Enter)");
       Console.ReadLine();
-      Loop();
-    }
-    public static int Max = 100;
-    public static int Min = 1;
+      int Max = 100;
+      int Min = 1;
+      int Count = 1;
+      var Loop = true;
+      while (Loop == true)
 
-    public static int Count = 1;
-    private static void Loop()
-    {
-
-
-      Random rdm = new Random();
-      int nbr = rdm.Next(Min, Max);
-
-
-      Console.WriteLine("Is your Number " + nbr + "? Higher? Lower? Yes!");
-      string Answer = Console.ReadLine();
-      if (Max == Min || Min == Max)
       {
-        Console.WriteLine("Liar");
-        Console.ReadLine();
-        return;
+        Random rdm = new Random();
+        int nbr = rdm.Next(Min, Max);
+        Console.WriteLine("Is your Number " + nbr + "? (Higher? Lower? Yes!)");
+        string Answer = Console.ReadLine();
+        if (Max == Min || Min == Max)
+        {
+          Console.WriteLine("Liar");
+          Console.ReadLine();
+          Loop = false;
+        }
+        if (Answer == "Higher")
+        {
+          Min = nbr + 1;
+          Count = Count + 1;
 
+        }
+
+        if (Answer == "Lower")
+        {
+          Max = nbr - 1;
+          Count = Count + 1;
+
+        }
+        if (Answer == "Yes")
+        {
+          Console.WriteLine("Your Number is " + nbr + "! It Took " + Count + " Guesses.");
+          Console.ReadLine();
+          Loop = false;
+          //   return;
+        }
+        if (Answer == "")
+        {
+          return;
+        }
       }
-      if (Answer == "Higher")
-      {
-        Min = nbr + 1;
-        Count = Count + 1;
-
-
-        Loop();
-      }
-
-      if (Answer == "Lower")
-      {
-        Max = nbr - 1;
-        Count = Count + 1;
-
-        Loop();
-
-      }
-      if (Answer == "Yes")
-      {
-        Console.WriteLine("Your Number is " + nbr + "! It Took " + Count + " Guesses.");
-        Console.ReadLine();
-        return;
-
-
-      }
-
     }
   }
 }
