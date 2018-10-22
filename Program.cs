9883ae4e229d800c272140285d42289260e04965
JOLee83 <lee.justin.oliver@gmail.com>
2018-10-22 17:03:19 -0400

    it works

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace BinarySearch
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Think of a number between 1 and 100. Ready? (Press Enter)");
+      Console.ReadLine();
+      Loop();
+    }
+    public static int Max = 100;
+    public static int Min = 1;
+
+    public static int Count = 1;
+    private static void Loop()
+    {
+
+
+      Random rdm = new Random();
+      int nbr = rdm.Next(Min, Max);
+
+
+      Console.WriteLine("Is your Number " + nbr + "? Higher? Lower? Yes!");
+      string Answer = Console.ReadLine();
+      if (Max == Min || Min == Max)
+      {
+        Console.WriteLine("Liar");
+        Console.ReadLine();
+        return;
+
+      }
+      if (Answer == "Higher")
+      {
+        Min = nbr + 1;
+        Count = Count + 1;
+
+
+        Loop();
+      }
+
+      if (Answer == "Lower")
+      {
+        Max = nbr - 1;
+        Count = Count + 1;
+
+        Loop();
+
+      }
+      if (Answer == "Yes")
+      {
+        Console.WriteLine("Your Number is " + nbr + "! It Took " + Count + " Guesses.");
+        Console.ReadLine();
+        return;
+
+
+      }
+
+    }
+  }
+}
